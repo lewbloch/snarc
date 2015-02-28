b50aa809f5156cf4373e118a5e57a99cb363ff20
Lew Bloch, KW2KW <kw2kw@lewscanon.com>
2015-02-28 13:05:42 -0800

    Start the repo.

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+Copyright © 2015, Lewis S. Bloch. All rights reserved.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>S.N.A.R.C. - the Secret Nonexistent Amateur Radio Club</title>
+    </head>
+
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div>
+            <h1>Welcome to S.N.A.R.C., the Secret Nonexistent Amateur Radio Club.</h1>
+        </div>
+        <div>
+            Check out our exciting projects.
+        </div>
+    </body>
+</html>
